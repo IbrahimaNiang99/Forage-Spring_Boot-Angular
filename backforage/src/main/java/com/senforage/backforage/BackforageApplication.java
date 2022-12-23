54e392f47f24e18431b021bea4f3f7c6048358c8
IbrahimaNiang99 <niang031999@gmail.com>
2022-12-23 21:39:24 +0000

    save

--- backforage/src/main/java/com/senforage/backforage/BackforageApplication.java
+++ backforage/src/main/java/com/senforage/backforage/BackforageApplication.java
@@ -1,14 +1,40 @@
 package com.senforage.backforage;
 
+import com.senforage.backforage.Entity.Role;
+import com.senforage.backforage.Entity.User;
+import com.senforage.backforage.Services.Userservice.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class BackforageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackforageApplication.class, args);
-        System.out.println("Bonjour Ibrahima NIANG");
     }
+    /*
+    @Bean
+    CommandLineRunner run(UserService userService){
+        return args -> {
+            userService.addRole(new Role(null, "ROLE_ADMIN"));
+            userService.addRole(new Role(null, "ROLE_GESCLIENTELE"));
+            userService.addRole(new Role(null, "ROLE_GESCOMMERCIALE"));
+            userService.addRole(new Role(null, "ROLE_GESCOMPTEUR"));
+
+            userService.addUser(new User(null, "Ibrahima Niang", "niang", "niang031999@gmail.com","passer", new ArrayList<>()));
+            userService.addUser(new User(null, "Mouhamed Fall", "fall", "fall@gmail.com","passer", new ArrayList<>()));
+            userService.addUser(new User(null, "Ndéye Awa Bâ", "awa", "awa@gmail.com","passer", new ArrayList<>()));
+            userService.addUser(new User(null, "Balla Diéne", "balla", "balla@gmail.com","passer", new ArrayList<>()));
+
+            userService.addRoleToUser("niang", "ROLE_ADMIN");
+            userService.addRoleToUser("fall", "ROLE_GESCLIENTELE");
+            userService.addRoleToUser("awa", "ROLE_GESCOMMERCIALE");
+            userService.addRoleToUser("balla", "ROLE_GESCOMPTEUR");
+        };
+    }*/
 
 }
---
+++ backforage/src/main/java/com/senforage/backforage/Security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.senforage.backforage.Security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration @AllArgsConstructor @EnableWebSecurity
+public class SecurityConfig{
+}
